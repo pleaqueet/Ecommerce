112fe9fa5caf5de8df702f770d2365d34ca8e792
pleaqueet <pleaqueet@yandex.ru>
2022-11-06 12:41:29 +0300

    Add Bottom Navigation View

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.ecommerce
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding.bottomNavigationView) {
+            setItemSelected(R.id.explorer)
+            replaceFragment(ExplorerFragment())
+            setOnItemSelectedListener { id ->
+                when (id) {
+                    R.id.explorer -> replaceFragment(ExplorerFragment())
+                    R.id.profile -> replaceFragment(ProfileFragment())
+                    R.id.favorites -> replaceFragment(FavoritesFragment())
+                    R.id.cart -> replaceFragment(CartFragment())
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.bottomNavigationView.getSelectedItemId() == R.id.explorer) {
+            super.onBackPressed()
+        } else {
+            binding.bottomNavigationView.setItemSelected(R.id.explorer)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment is ExplorerFragment) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment).commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment).commit()
+        }
     }
 }
